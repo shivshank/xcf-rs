778182101795897202a1df9566a9c20e02a4d69c
dxenonb <drdxbme@gmail.com>
2023-03-23 15:22:45 -0400

    Update docs

--- src/lib.rs
+++ src/lib.rs
@@ -34,6 +34,10 @@ pub enum Error {
 #[derive(Debug)]
 pub struct Xcf {
     pub header: XcfHeader,
+    /// List of layers in the XCF file, in the order they are stored in the file.
+    /// (I believe this is top layer to bottom layer)
+    ///
+    /// See [`Xcf::layer`](Xcf::layer) to get a layer by name.
     pub layers: Vec<Layer>,
 }
 
@@ -83,10 +87,12 @@ impl Xcf {
         (self.width(), self.height())
     }
 
+    /// Get a reference to a layer by `name`.
     pub fn layer(&self, name: &str) -> Option<&Layer> {
         self.layers.iter().find(|l| l.name == name)
     }
 
+    /// Get a mutable reference to a layer by `name`.
     pub fn layer_mut(&mut self, name: &str) -> Option<&mut Layer> {
         self.layers.iter_mut().find(|l| l.name == name)
     }
